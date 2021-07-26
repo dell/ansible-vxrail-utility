a9ee33375f0ad6bc505fb6f3e399f9e49a411973
Salinda <gaoh8@dell.com>
2021-07-26 17:28:25 +0800

    Added Day1 APIs

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "vxrail_ansible_utility"
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 # To install the library, run the following
 #
 # python setup.py install
@@ -26,14 +26,14 @@ REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 setup(
     name=NAME,
     version=VERSION,
-    description="VxRail Cluster and System Management",
+    description="VxRail Cluster,System Management and Day1 bring up",
     author_email="",
     url="",
-    keywords=["Swagger", "VxRail Cluster and System Management"],
+    keywords=["Swagger", "VxRail Cluster,System Management and Day1 bring up"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description="""\
-    APIs for cluster management and system management  # noqa: E501
+    APIs for cluster management,system management and Day1 bring up # noqa: E501
     """
 )
